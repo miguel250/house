aac5b9f4d22e87e1c8267b6680e396d7512de4e6
Miguel Perez <miguel250@gmail.com>
2013-11-09 21:18:00 -0500

    Fix coding standard.

--- core/module/Api/Module.php
+++ core/module/Api/Module.php
@@ -6,63 +6,63 @@ use Zend\Mvc\MvcEvent;
 use Zend\View\Model\JsonModel;
 class Module
 {
-       public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
+  public function onBootstrap(MvcEvent $e)
+  {
+    $eventManager        = $e->getApplication()->getEventManager();
+    $moduleRouteListener = new ModuleRouteListener();
+    $moduleRouteListener->attach($eventManager);
 
-        $eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, array($this, 'onDispatchError'), 0);
-        $eventManager->attach(MvcEvent::EVENT_RENDER_ERROR, array($this, 'onRenderError'), 0);
+    $eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, array($this, 'onDispatchError'), 0);
+    $eventManager->attach(MvcEvent::EVENT_RENDER_ERROR, array($this, 'onRenderError'), 0);
+  }
+
+  public function onDispatchError($e)
+  {
+    return $this->getJsonModelError($e);
+  }
+
+  public function onRenderError($e)
+  {
+    return $this->getJsonModelError($e);
+  }
+
+  public function getJsonModelError($e)
+  {
+    $error = $e->getError();
+    if (!$error) {
+      return;
     }
 
-   public function onDispatchError($e)
-       {
-           return $this->getJsonModelError($e);
-       }
+    $response = $e->getResponse();
+    $exception = $e->getParam('exception');
+    $exceptionJson = array();
+    if ($exception) {
+        $exceptionJson = array(
+            'class' => get_class($exception),
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine(),
+            'message' => $exception->getMessage(),
+            'stacktrace' => $exception->getTraceAsString()
+        );
+    }
 
-       public function onRenderError($e)
-       {
-           return $this->getJsonModelError($e);
-       }
+    $errorJson = array(
+        'message'   => 'An error occurred during execution; please try again later.',
+    );
 
-       public function getJsonModelError($e)
-       {
-           $error = $e->getError();
-           if (!$error) {
-               return;
-           }
+    if($exception){
+     $errorJson['message'] = $exception->getMessage();
+    }
+    if ($error == 'error-router-no-match') {
+        $errorJson['message'] = 'Resource not found.';
+    }
 
-           $response = $e->getResponse();
-           $exception = $e->getParam('exception');
-           $exceptionJson = array();
-           if ($exception) {
-               $exceptionJson = array(
-                   'class' => get_class($exception),
-                   'file' => $exception->getFile(),
-                   'line' => $exception->getLine(),
-                   'message' => $exception->getMessage(),
-                   'stacktrace' => $exception->getTraceAsString()
-               );
-           }
+    $model = new JsonModel($errorJson);
 
-           $errorJson = array(
-               'message'   => 'An error occurred during execution; please try again later.',
-           );
+    $e->setResult($model);
 
-           if($exception){
-            $errorJson['message'] = $exception->getMessage();
-           }
-           if ($error == 'error-router-no-match') {
-               $errorJson['message'] = 'Resource not found.';
-           }
-
-           $model = new JsonModel($errorJson);
-
-           $e->setResult($model);
-
-           return $model;
-       }
+    return $model;
+  }
 
     public function getConfig()
     {
